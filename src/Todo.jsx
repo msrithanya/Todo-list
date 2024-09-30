d970a207f18fec0886eabfdd98d29e943e9f4f67
msrithanya <rithanyamanoharan100@gmail.com>
2024-09-30 23:33:16 +0530

    File changes

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useRef } from 'react';
+import './Todo.css';
 function Todo() {
   const [valu, setValue] = useState([]);
-  const add = () => {
-    const newValu = [...valu, { data: "" }];
+  const blue=useRef(null);
+  const textColor=useRef(null);
+  const add = (num) => {
+    const newValu = [...valu, { data: "",color:num}];
     setValue(newValu);
-    localStorage.setItem("value", JSON.stringify(valu)); 
+    localStorage.setItem("value", JSON.stringify(newValu)); // Use the new array
   };
+
   const con = (e, i) => {
     const content = e.target.value;
     const newa = [...valu];
@@ -13,25 +17,47 @@ function Todo() {
     setValue(newa);
     localStorage.setItem("value", JSON.stringify(newa));
   };
+
   useEffect(() => {
     const storedValu = JSON.parse(localStorage.getItem("value"));
     if (storedValu) {
       setValue(storedValu);
     }
   }, []);
+
+  const dele = (i) => {
+    const newArr = valu.filter((_, index) => index != i);
+    setValue(newArr);
+    localStorage.setItem("value", JSON.stringify(newArr));
+  };
+const colors=()=>{
+  if(  blue.current.style.display=="block"){
+     blue.current.style.display="none"
+  }
+  else{
+  blue.current.style.display="block"
+  }
+
+}
   return (
     <div>
-      <button onClick={add}>Add</button>
+      <button className='add_button' onClick={colors}>+</button>
+      <div ref={blue} onClick={()=>add(1)} className='blue'>
+      </div>
       {valu?.map((v, i) => (
-        <textarea
-          key={i}
-          placeholder="Type something"
-          onChange={(e) => con(e, i)}>
-            {v.data}
-          </textarea>
+        <div className='container' key={i}>
+          <textarea ref={ textColor}
+            placeholder="Type something"
+            onChange={(e) => con(e, i)}
+            value={v.data}
+             style={{ backgroundColor: v.color === 1 ? 'blue' : 'black' }}
+          />
+          <button onClick={() => dele(i)}>Delete</button>
+        </div>
       ))}
     </div>
   );
 }
 
 export default Todo;
+
